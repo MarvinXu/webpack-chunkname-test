0c837257580fcf1a65f377900f1abf7f38dda282
xupeiyuan <xupeiyuan@58ganji.com>
2017-05-15 14:27:13 +0800

    add require.ensure test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const Hello = () => import(/* webpackChunkName: "hello" */'@/components/Hello')
+// const Hello = () => import( /*webpackChunkName: "hello"*/ '@/components/Hello')
+const Hello = require.ensure([], function (require) {
+	require('@/components/Hello')
+}, 'hello')
 import(/* webpackChunkName: "chunk1" */'@/components/chunk1.js')
 Vue.use(Router)
 
