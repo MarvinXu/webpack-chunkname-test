9eb06df16e34a80bd749ab986a2a1c08f84d60f1
xupeiyuan <xupeiyuan@58ganji.com>
2017-06-01 14:31:24 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // const Hello = () => import( /*webpackChunkName: "hello"*/ '@/components/Hello')
+
+// require.ensure naming works fine
 const Hello = require.ensure([], function (require) {
 	require('@/components/Hello')
 }, 'hello')
-import(/* webpackChunkName: "chunk1" */'@/components/chunk1.js')
+
+
+// import naming doesn't work
+// I removed alias '@' for test
+import(/* webpackChunkName: "chunk1" */'../components/chunk1.js')
+
 Vue.use(Router)
 
 export default new Router({
